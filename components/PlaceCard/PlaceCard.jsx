080a7ff3907211d5862020ca2a78d496a22ae981
jesus doza <jesus.doza@gmail.com>
2023-07-08 13:28:48 -0500

    Add placeCard.jsx

--- components/PlaceCard/PlaceCard.jsx
+++ components/PlaceCard/PlaceCard.jsx
@@ -1,19 +1,27 @@
-import { View, Text } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
 import React from "react";
+import { Button, Card, Text } from "react-native-paper";
 
-export default function PlaceCard() {
-    /*
-    const coordinates= []
-    const id = ''
-    const name=''
-    const type="Feature"
-    const kinds = "kinds": "historic,monuments_and_memorials,urban_environment,cultural,interesting_places,sculptures,monuments",
-
-    */
+export function PlaceCard({ placeData }) {
+    console.log("placeData", placeData);
     return (
-        <View>
-            <Text>PlaceCard</Text>
-        </View>
+        // <View style={styles.padding}>
+        //     <Text>{placeData.id}</Text>
+        //     <Text>{placeData.name}</Text>
+        //     <Text>{placeData.type}</Text>
+        // </View>
+        <Card>
+            {/* <Card.Title title={placeData.name} subtitle={placeData.name} /> */}
+            <Card.Content>
+                <Text variant="titleLarge">{placeData.name}</Text>
+                <Text variant="bodyMedium">{placeData.type}</Text>
+            </Card.Content>
+            <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+            <Card.Actions>
+                <Button>Back To City</Button>
+                <Button>Go To</Button>
+            </Card.Actions>
+        </Card>
     );
 }
 
@@ -38,3 +46,10 @@ sample object
         "type": "Feature"
     },
 */
+
+const styles = StyleSheet.create({
+    padding: {
+        padding: "1%",
+        height: "40%",
+    },
+});
